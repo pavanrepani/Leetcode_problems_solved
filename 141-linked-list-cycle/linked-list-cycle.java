7d8310d9586b522290ed3708b6f3d27138783c11
pavan111 <pavanrepani111@gmail.com>
2026-08-15 22:11:54 +0530

    Time: 0 ms (100.00%) | Memory: 46.9 MB (33.82%) - LeetSync

--- 141-linked-list-cycle/linked-list-cycle.java
+++ 141-linked-list-cycle/linked-list-cycle.java
@@ -14,14 +14,14 @@ public class Solution {
         if(head == null){
             return false;
         }
-        HashSet<ListNode> set = new HashSet<>();
-        while(head!=null){
-            if(set.contains(head)){
+        ListNode slow = head;
+        ListNode fast = head;
+        while(fast!=null && fast.next!=null){
+            slow = slow.next;
+            fast = fast.next.next;
+            if(slow == fast){
                 return true;
             }
-            set.add(head);
-            head = head.next;
-            
         }
         return false;
 
